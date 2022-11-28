43fb4c58d36639dfb3e1675f7b067ead9cfb6402
Sone4ka <treninasonya@yandex.ru>
2022-11-28 14:41:18 +0300

    Вторая версия, кружочки разного цвета

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 from random import sample
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
 class Example(QWidget):
@@ -12,9 +11,13 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        uic.loadUi('UI.ui', self)
+        self.setGeometry(300, 300, 400, 400)
+        self.setWindowTitle('UI')
         self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
+        self.book = QPushButton('Создать кружочки!', self)
+        self.book.resize(self.book.sizeHint())
+        self.book.move(150, 10)
+        self.book.clicked.connect(self.paint)
 
     def paintEvent(self, event):
         if self.do_paint:
@@ -32,7 +35,8 @@ class Example(QWidget):
         d2 = sample(range(1, 150), 1)[0]
         d3 = sample(range(1, 150), 1)[0]
         d4 = sample(range(1, 150), 1)[0]
-        qp.setBrush(QColor(255, 255, 0))
+        color = sample(range(0, 255), 3)
+        qp.setBrush(QColor(*color))
         qp.drawEllipse(30, 30, d1, d1)
         qp.drawEllipse(330, 250, d2, d2)
         qp.drawEllipse(70, 10, d3, d3)
